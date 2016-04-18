f2b4112258634e1b5891e647e878e83ae04cf247
Abby <abigaillyons@college.harvard.edu>
2016-04-17 21:40:22 -0400

    kk finished

--- kk.c
+++ kk.c
@@ -1,12 +1,20 @@
 // kk.c
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define ITERS 25000
 #define SIZE 100
+
+// from c++reference
+int compare (const void * a, const void * b)
+{
+  	return ( *(int*)b - *(int*)a );
+}
 
 // from http://stackoverflow.com/questions/7920860/how-to-generate-large-random-numbers-c
 uint64_t gen() 
@@ -44,8 +52,28 @@ int main(int argc, char * argv[])
 // Karmarkar-Karp
 unsigned int kk(uint64_t array[])
 {
+	// sort in reverse order
 	uint64_t * diffArray = (uint64_t *) malloc(SIZE * sizeof(uint64_t));
-	return 0;
+	memcpy(diffArray, array, SIZE * sizeof(uint64_t));
+	qsort(diffArray, SIZE, sizeof(uint64_t), compare);
+
+	// recursively find differences
+	while (true)
+	{
+		for (unsigned int i = 0; i < SIZE - 1; i += 2)
+		{
+			if (diffArray[i] == 0)  break;	
+			diffArray[i] = diffArray[i] - diffArray[i + 1];
+			diffArray[i + 1] = 0;
+		}
+		qsort(diffArray, SIZE, sizeof(uint64_t), compare);
+		if (diffArray[1] == 0)  break;
+	}
+
+	// return best
+	uint64_t best = diffArray[0];
+	free(diffArray);
+	return best;
 }
 
 // Repeated random
