110b737560a32e8b712f59350242828ca44bfc80
Abby <abigaillyons@college.harvard.edu>
2016-04-17 20:33:45 -0400

    structure added

--- main.c
+++ main.c
@@ -6,3 +6,27 @@ int main(void)
 {
 	printf("Hello world!\n");
 }
+
+// Karmarkar-Karp
+unsigned int kk()
+{
+	
+}
+
+// Repeated random
+unsigned int random()
+{
+	
+}
+
+// Hill-climbing
+unsigned int hc()
+{
+	
+}
+
+// Simulated annealing
+unsigned int sa()
+{
+	
+}
